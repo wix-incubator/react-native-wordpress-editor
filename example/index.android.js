8a8b0ca98b765392ed5eeb7d852ffab8d11ea7f8
Yedidya Kennard <yedidyak@wix.com>
2016-07-24 13:09:53 +0300

    JS using android in example

--- example/index.android.js
+++ example/index.android.js
@@ -6,29 +6,51 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
 } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import EditorScreen from './EditorScreen';
+import WelcomeScreen from './WelcomeScreen';
 
 class example extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
     );
   }
 }
+
+
+Navigation.registerComponent('example', () => example);
+
+
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'example',
+    navigatorStyle: {
+      navBarTextColor: '#ffffff',
+      navBarButtonColor: '#ffffff',
+      navBarBackgroundColor: '#00a0d2'
+    },
+    title: 'ANDROID!'
+  }
+});
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -49,4 +71,3 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('example', () => example);
